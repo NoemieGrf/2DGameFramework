70c0d0d8cb62c12572dac77565111a3642b39513
Noemie <grf.noemie@gmail.com>
2024-02-16 22:48:48 +0800

    Configure SFML static library!

--- Framework/src/Main.cpp
+++ Framework/src/Main.cpp
@@ -1,8 +1,24 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout << "Hello world!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(1000, 1200), "Test");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-	return 0;
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
