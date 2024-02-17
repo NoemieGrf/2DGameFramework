4030731e08c3554eca07ae357598180ca52b43cb
Noemie <134601774+NoemieGrf@users.noreply.github.com>
2024-02-17 14:19:56 +0800

    Add a player and make it move!

--- Framework/src/Main.cpp
+++ Framework/src/Main.cpp
@@ -1,13 +1,32 @@
 #include <SFML/Graphics.hpp>
+#include <algorithm>
+
+using uint = unsigned int;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1000, 1200), "Test");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    uint screenHeight = sf::VideoMode::getDesktopMode().height;
+    uint screenWidth = sf::VideoMode::getDesktopMode().width;
+
+    uint minSize = std::min(screenHeight, screenWidth);
+    uint windowSize = 0.7 * minSize;
+
+    sf::RenderWindow window(sf::VideoMode(windowSize, windowSize), "Test");
+    
+    sf::Texture tex;
+    tex.loadFromFile("./assets/player.png");
+
+    sf::Sprite sp(tex);
+    sp.setScale(0.5f, 0.5f);
+    
+    float spPosX = 100;
+    float spPosY = 100;
+    
+    
 
     while (window.isOpen())
     {
+        // Window event
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -15,8 +34,34 @@ int main()
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        // User input
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+        {
+            spPosY -= 0.1f;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+        {
+            spPosY += 0.1f;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+        {
+            spPosX -= 0.1f;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+        {
+            spPosX += 0.1f;
+        }
+
+
+
+
+        sp.setPosition(spPosX, spPosY);
+
+
+
+        // Render 
+        window.clear(sf::Color::White);
+        window.draw(sp);
         window.display();
     }
 
