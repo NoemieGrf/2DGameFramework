cbd03e5427c0b44181e6b4470762b7dc223e25d3
Noemie <grf.noemie@gmail.com>
2024-03-03 19:08:16 +0800

    Update physics.

--- src/Component/Impl/CompCollider.cpp
+++ src/Component/Impl/CompCollider.cpp
@@ -1,12 +1,33 @@
 #include "CompCollider.h"
 #include "../../Game/Game.h"
+#include "CompTransform.h"
 
-void CompCollider::Init()
+void CompCollider::Init(bool isDynamic, const vec2f& aabbBox)
 {
     b2BodyDef bodyDef;
-    bodyDef.type = b2_dynamicBody;
-    bodyDef.position.Set(0.0f, 0.0f);
+    bodyDef.type = isDynamic ? b2_dynamicBody: b2_staticBody;
+    bodyDef.fixedRotation = true;
+
+    const CompTransform* pEntityTrans = GetEntity()->GetComponent<CompTransform>();
+    if (pEntityTrans != nullptr)
+    {
+        auto transWorldPos = pEntityTrans->GetPosition();
+        bodyDef.position.Set(transWorldPos.x, transWorldPos.y); // todo move to center
+    }
+    else
+        bodyDef.position.Set(0.0f, 0.0f);
 
     b2Body* _pPhyBodyRaw = Game::GetManager<PhysicsManager>()->GetPhysicWorld()->CreateBody(&bodyDef);
     _pPhyBody = std::unique_ptr<b2Body, PhysicsManager::B2BodyDeleter>(_pPhyBodyRaw);
+
+    b2PolygonShape boxShape;
+    boxShape.SetAsBox(aabbBox.x, aabbBox.y);    // box size
+
+    b2FixtureDef fixtureDef;
+    fixtureDef.shape = &boxShape;
+    fixtureDef.density = 1.0f; // 设置密度，影响物体的质量
+    fixtureDef.friction = 0.3f; // 设置摩擦系数
+    fixtureDef.restitution = 0.0f;  // 设置弹性
+
+    _pPhyBody->CreateFixture(&fixtureDef);
 }
--- src/Component/Impl/CompCollider.h
+++ src/Component/Impl/CompCollider.h
@@ -8,7 +8,7 @@
 class CompCollider: public ComponentTypeGetter<ComponentType::Collider>
 {
 public:
-    auto Init() -> void;
+    auto Init(bool isDynamic, const vec2f& aabbBox) -> void;
 
 public:
     std::unique_ptr<b2Body, PhysicsManager::B2BodyDeleter> _pPhyBody = nullptr;
